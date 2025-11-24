4f1228c338da11b4b00863dd5225bc0f64843305
matibestofall-png <matibestofall@gmail.com>
2025-11-24 08:17:48 +0000

    Finalne2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
 
-class Choinka {
-    public static void draw(int height, char c) {
-        for (int i = 0; i < height; i++) {
-            int stars =  i + 1;
-            int spaces = height - i - 1;
-            for (int s = 0; s <= height; s++) System.out.print(' ');
-            for (int s = 0; s < stars; s++) System.out.print(c);
-            System.out.println();
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Podaj wysokość trójkąta (np. 5): ");
-        int height = 0;
-        try { height = Integer.parseInt(sc.nextLine().trim()); }
-        catch (Exception e) { height = 5; }
+
+        System.out.print("Podaj liczbę trójkątów: ");
+        int count = Integer.parseInt(sc.nextLine().trim());
+
+        System.out.print("Podaj wysokość trójkąta: ");
+        int height = Integer.parseInt(sc.nextLine().trim());
 
         System.out.print("Podaj znak do rysowania (np. *): ");
         String znak = sc.nextLine();
         if (znak.isEmpty()) znak = "*";
         char c = znak.charAt(0);
 
-        Choinka.draw(height, c);
+        
+        for (int t = 1; t <= count; t++) {
+            for (int i = 1; i <= height; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(c);
+                }
+                System.out.println();
+            }
+            System.out.println(); 
+        }
+
         sc.close();
     }
 }
